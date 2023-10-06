fd1b2e8e0c4286bb72c7f16ce0429d60784aeac7
Zura <jur.and.jr42@gmail.com>
2023-10-06 17:27:25 -0300

    Created RegistrationService and RegistrationRequest classes

--- src/main/java/com/joi/backendProject/controller/RegistrationController.java
+++ src/main/java/com/joi/backendProject/controller/RegistrationController.java
@@ -1,5 +1,15 @@
 package com.joi.backendProject.controller;
 
+import com.joi.backendProject.service.RegistrationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RegistrationController {
-    
+    @Autowired
+    private RegistrationService registrationService;
+    public String register(@RequestBody RegistrationRequest request) {
+        return registrationService.register(request);
+    }
 }

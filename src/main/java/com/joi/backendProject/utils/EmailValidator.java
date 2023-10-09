4b8c0b5e3ec6964a5e298099f89c5cead5ff8854
Zura <jur.and.jr42@gmail.com>
2023-10-09 15:40:16 -0300

    Implemented regex validation for email

--- src/main/java/com/joi/backendProject/utils/EmailValidator.java
+++ src/main/java/com/joi/backendProject/utils/EmailValidator.java
@@ -5,14 +5,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 @Service
 public class EmailValidator implements Predicate<String> {
 
+    private static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@(.+)$";
+    private final Pattern pattern = Pattern.compile(EMAIL_REGEX);
+
     @Override
-    public boolean test(String s) {
-        // TODO: Regex to validate email
-        return true;
+    public boolean test(String email) {
+        if (email == null || email.isEmpty()) {
+            return false;
+        }
+        return pattern.matcher(email).matches();
     }
 
 }

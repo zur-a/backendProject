f707bcd8b7b253ae91f5615c1f0a2c5b6a52922a
Zura <jur.and.jr42@gmail.com>
2023-10-09 10:40:10 -0300

    Created utils package and EmailValidator class

--- src/main/java/com/joi/backendProject/controller/RegistrationController.java
+++ src/main/java/com/joi/backendProject/controller/RegistrationController.java
@@ -3,6 +3,7 @@ package com.joi.backendProject.controller;
 import com.joi.backendProject.request.RegistrationRequest;
 import com.joi.backendProject.service.RegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class RegistrationController {
     @Autowired
     private RegistrationService registrationService;
+
+    @PostMapping("/api/v1/registration")
     public String register(@RequestBody RegistrationRequest request) {
         return registrationService.register(request);
     }
--- src/main/java/com/joi/backendProject/service/RegistrationService.java
+++ src/main/java/com/joi/backendProject/service/RegistrationService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 public class RegistrationService {
 
     public String register(RegistrationRequest request) {
+
         return "muda muda muda";
     }
 }

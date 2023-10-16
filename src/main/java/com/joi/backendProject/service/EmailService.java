17a77d97b7203286b1c58741e7f1980e523094e7
Zura <jur.and.jr42@gmail.com>
2023-10-16 15:20:33 -0300

    Implemented email sender logic to EmailService

--- src/main/java/com/joi/backendProject/service/EmailService.java
+++ src/main/java/com/joi/backendProject/service/EmailService.java
@@ -1,5 +1,40 @@
 package com.joi.backendProject.service;
 
-public class EmailService {
-    
+import com.joi.backendProject.email.EmailSender;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService implements EmailSender {
+
+    private final String CONFIRM_MESSAGE = "Confirm your email";
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Autowired
+    private static Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
+
+    @Async
+    @Override
+    public void send(String recipient, String email) {
+        try {
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, "utf-8");
+            helper.setText(email, true);
+            helper.setTo(recipient);
+            helper.setSubject(CONFIRM_MESSAGE);
+            helper.setFrom("ozeroearosa@gmail.com");
+        } catch (MessagingException e) {
+            LOGGER.error("Error, email not sent ", e);
+            throw new IllegalStateException("Failed to sent the email");
+        }
+    }
 }
